7bad1fd2f66dce23fcda2ea9d65094d34c1c5b52
martinbeckmann <m.beckmann123@web.de>
2017-10-29 15:03:55 +0100

    Tests fertig, Wrapper fix

--- Praktikum 1/wrapper/src/wrapper/ISensor.java
+++ Praktikum 1/wrapper/src/wrapper/ISensor.java
@@ -3,37 +3,27 @@ package wrapper;
 import java.io.IOException;
 import javax.swing.plaf.basic.BasicSliderUI.ActionScroller;
 
-public class ISensor
-{
+public class ISensor {
     private ISensorAPI _sensor;
 
-    public ISensor(ISensorAPI sensor)
-    {
+    public ISensor(ISensorAPI sensor) {
         _sensor = sensor;
     }
-
     
-    
-    public int getDistance() throws IOException
-    {
+    public int getDistance() throws IOException {
         _sensor.t();
         char sensorOut = _sensor.d();
         int distance = 0;
-        if(sensorOut > 255 || sensorOut < 0)
-        {
-            throw new IOException("Sensordaten ungültig: " + sensorOut);
+        if (sensorOut > 255 || sensorOut < 0) {
+            throw new IOException("Sensordaten ungï¿½ltig: " + sensorOut);
+        } else {
+            distance = (int) (Math.acos(1.0 - ((sensorOut + 5.0) / 265.0)) * 60.0 / Math.PI);
+            if (distance < 0) {
+                distance = 0;
+            }
         }
-        else
-        {
-        distance = (int) (Math.acos(1 - ((sensorOut + 5) / 265)) * 60/Math.PI);    
-        if(distance < 0)
-        {
-            distance = 0;
-        }
-        
-        }
-        
-        
+
+
         return distance;
-    };
+    }
 }
