580de1470a81a1e1cd065c156b8a0b6f6087bfa7
Maximilian Mang <maximilian.mang@haw-hamburg.de>
2017-11-01 21:00:09 +0100

    Mocksensor kommentare angefügt

--- Praktikum 1/wrapper/src/wrapper/MockSensor.java
+++ Praktikum 1/wrapper/src/wrapper/MockSensor.java
@@ -1,16 +1,31 @@
 package wrapper;
-
+/**
+ * Diese Klasse emuliert einen Abstandssensor, der Werte zwischen 0 und 255 zurückgibt.
+ * @author BCK
+ *
+ */
 public class MockSensor implements ISensorAPI{
     private char _testValue;
 
+    /**
+     * Setzt einen zu erzeugenden Abstandswert, der im Sensor gespeichert wird
+     * @param Ein bestimmter distanz Wert 0-255
+     */
     public void setTestValue(int discreteValue){
         _testValue = (char) discreteValue;
     }
 
+    /**
+     * Gibt den aktuell emulierten Messwert zurück
+     * @return Ein Distanzwert zwischen 0 und 255
+     */
     public char d(){
         return _testValue;
     };
 
+    /**
+     * Macht nichts, aber implementiert dadurch das Interface
+     */
     public void t(){
 
     }
